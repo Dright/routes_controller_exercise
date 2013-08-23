25aa6b50b780883a070d222d90d27bff106e5286
Dright <as58140399@gmail.com>
2013-08-23 16:23:39 +0800

    users can create new product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  validates :name, :presence => true, :uniqueness => true
+  validates :price, :presence => true
+
   belongs_to :user
   attr_accessible :name, :price
 end
